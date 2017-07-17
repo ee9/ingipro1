c66e241263157a5c2c551680ea4067a883d60c99
Egor <eerofeev9@gmail.com>
2017-07-17 10:40:11 +0300

    commit

--- 3510.js
+++ 3510.js
@@ -3,16 +3,13 @@
  */
 const arr1 = [1, 2, 3, 4, 5];
 const arr2 = [1, 5, 6, 7];
-var newArr = arr1.concat( arr2 );
-alert ( newArr );
+const newArr = arr1.concat( arr2 );
+//alert ( newArr );
 
-function unique(arr) {
+function unique(newArr) {
     var obj = {};
 
-    for (var i = 0; i < arr.length; i++) {
-        var str = arr[i];
-        obj[str] = true;
-    }
+    newArr.forEach((value) => obj[value] = true );
 
     return Object.keys(obj);
 };
