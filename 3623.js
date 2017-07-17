28301b765463d1cd9b3b7f889cce08291762c99f
ee9 <eerofeev9@gmail.com>
2017-07-17 08:59:52 +0300

    Update 3623.js

--- 3623.js
+++ 3623.js
@@ -5,7 +5,7 @@ var arr = [
     {name: 'width', value: 10},
     {name: 'height', value: 20}
 ] ;
-var res = arr.reduce(function(previousValue, currentValue, index, array) {
+var res = arr.reduce(function(previousValue, currentValue) {
     previousValue[currentValue.name] = currentValue.value;
     return previousValue;
 }, {})
